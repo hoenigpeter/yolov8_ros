a342a8fd7f7c24a026781a2a8d753b9ec11d7111
Peter Hoenig <hpfreak1004@gmail.com>
2025-05-09 19:58:17 +0200

    dockerfile pip versioning fix

--- test_image.py
+++ test_image.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load the pre-trained YOLOv10-N model
-model = YOLO("yolov8l-oiv7.pt")
-results = model("test_img_2.jpg")
+model = YOLO("./runs/segment/train_camera2/weights/last.pt")
+results = model("/ssd3/real_camera_dataset/real_test/scene_2/0000_color.png")
 results[0].show()
 results[0].save(filename="test_result.jpg") 
